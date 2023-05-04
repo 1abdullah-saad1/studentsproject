56bc5967b2021e5c9a7289e91041ad0d02e55e93
1abdullah-saad1 <53198430+1abdullah-saad1@users.noreply.github.com>
2023-05-04 20:48:33 +0300

    Update StudentEnroleCourse.php

--- app/Models/StudentEnroleCourse.php
+++ app/Models/StudentEnroleCourse.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class StudentEnroleCourse extends Model
 {
     use HasFactory;
+    /**
+     * Get the student that owns the StudentEnroleCourse
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'id');
+    }
+    /**
+     * Get the stage that owns the StudentEnroleCourse
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function stage()
+    {
+        return $this->belongsTo(Stages::class, 'stage_id', 'id');
+    }
+    /**
+     * Get the academic_year that owns the StudentEnroleCourse
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function academic_year()
+    {
+        return $this->belongsTo(AcademicYears::class, 'academic_year_id', 'id');
+    }
 }
